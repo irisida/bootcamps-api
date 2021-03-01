f7f9205cc84124a870740165a80d47a8a6e8dba9
irisida <20445648+irisida@users.noreply.github.com>
2021-03-01 17:52:17 +0000

    error middleware update

--- middleware/error.js
+++ middleware/error.js
@@ -5,14 +5,47 @@ const errorHandler = (err, req, res, next) => {
 
   error.message = err.message;
 
-  console.log(err.stack.red);
+  console.log(err);
 
-  // mongoose bad ObjectId
+  /**
+   * Error case handler for a bad ObjectId
+   * sets message,
+   * creates new ErrorResponse object with message and
+   * a statsCode for notFound, 404
+   */
   if (err.name === 'CastError') {
     const message = `Resource not found with ID of ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
 
+  /**
+   * Error case handler for a duplicate key field.
+   * sets message,
+   * creates new ErrorResponse object with message and
+   * a statsCode for badRequest, 400
+   */
+  if (err.code === 11000) {
+    const message =
+      'Duplicate value in key field for Resource where unique value is expected';
+    error = new ErrorResponse(message, 400);
+  }
+
+  /**
+   * Error case handler for validation error(s)
+   * sets message,
+   * creates new ErrorResponse object with message and
+   * a statsCode for badRequest, 400
+   */
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  /**
+   * deault handler
+   * sets status or default
+   * sets message or default
+   */
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error',
