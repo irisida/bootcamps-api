afc60f0e5f369dffc16cb3e05706112f48044ade
irisida <20445648+irisida@users.noreply.github.com>
2021-03-04 17:55:39 +0000

    added pagination handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -56,11 +56,43 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt');
   }
 
+  /**
+   * pagination handling.
+   * defines the page number, the limit per page is defaulted to
+   * 100 and we also define the skip.
+   */
+  console.log('req.query.page:', req.query.page);
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 1;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   const bootcamps = await query;
+
+  // pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
 
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination: pagination,
     data: bootcamps,
   });
 });
