8d968c2318f5d242c6b34cc1ed0c4ecf7199fcbc
irisida <20445648+irisida@users.noreply.github.com>
2021-03-05 20:49:28 +0000

    added the ability to add a course to a bootcamp

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 const { json } = require('express');
 
 /**
@@ -50,6 +51,34 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
   if (!course) {
     return next(new ErrorResponse(`No course with id: ${req.params.id}`), 404);
   }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+/**
+ * @description     POST a new course with details
+ * @route           POST /api/v1/bootcamps/:id/courses
+ * @access          Private
+ * @param {*} req   request
+ * @param {*} res   response
+ * @param {*} next  next functions
+ */
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootamp = req.params.bootcampId;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No bootcamp with id: ${req.params.bootcampId}`),
+      404
+    );
+  }
+
+  const course = await Course.create(req.body);
 
   res.status(200).json({
     success: true,
