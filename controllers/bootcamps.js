59d9d0662ef22c1e6f0005f7251e5e7fd73d55e8
irisida <20445648+irisida@users.noreply.github.com>
2021-03-07 20:46:32 +0000

    added users to seeder, added constraints to admin, publisher and users ability to post bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -46,6 +46,22 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
  * @param {*} next  next functions
  */
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // add user to req.body
+  req.body.user = req.user.id;
+
+  // check for published bootcamps from this user
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // if user is not an admin, only able to add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The user with id ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
 
   res.status(201).json({
--- seeder.js
+++ seeder.js
@@ -13,6 +13,7 @@ dotenv.config({ path: './config/config.env' });
  */
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 /**
  * DB connection
@@ -27,6 +28,10 @@ mongoose.connect(process.env.MONGO_URI, {
 /**
  * read the JSON files
  */
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
@@ -40,6 +45,7 @@ const courses = JSON.parse(
  */
 const importData = async () => {
   try {
+    await User.create(users);
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
 
@@ -57,6 +63,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log(`Seed Data deleted...`.red.inverse);
     process.exit();
